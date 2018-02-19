b07d45e1813e419833e48e79aa052bcb94d2846e
Ariana Berger <ariana.berger@gmail.com>
2018-02-19 01:05:47 +0000

    first commit

---
+++ config/environment.rb
@@ -0,0 +1,10 @@
+#this file is required by wine_and_dine, add all requirement here!
+
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+require 'colorize'
+
+require_relative '../lib/comedy_shows/shows'
+require_relative '../lib/comedy_shows/scraper'
+require_relative '../lib/comedy_shows/cli'
---
+++ lib/version.rb
@@ -0,0 +1,3 @@
+module ComedyShows
+  VERSION = "0.0.0"
+end
