7260d1991d47a3073fe5b320d2e0fb898de3c558
arianaberger <ariana.berger@gmail.com>
2018-02-22 10:53:25 -0500

    Update shows.rb

--- lib/comedy_shows/shows.rb
+++ lib/comedy_shows/shows.rb
@@ -9,14 +9,15 @@ class ComedyShows::Shows
     @@all << self
   end
 
-  def self.create_shows(shows_array) #creates array of show instances from the main schedule page using the scraper hash
+  def self.create_shows(shows_array) #creates show instances from the main schedule page using the scraper hash and pushes them into @@all
     shows_array.each do |s|
       ComedyShows::Shows.new(s)
     end
   end
 
-  def add_show_details(details_hash)
-    details_hash.each{|k,v| self.send("#{k}=",v)}
+  def add_show_details(details_hash) #perhaps modify to only use on individual shows when called by the cli?
+    # details_hash.each{|k,v| self.send("#{k}=",v)}
+    ComedyShows::Shows.new(details_hash)
   end
 
   def self.all
