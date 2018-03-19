5dcdcff0b225cd529c57f26a0d7b1af5f61c1a8e
Ariana Berger <ariana.berger@gmail.com>
2018-03-19 16:49:18 +0000

    minor refactoring with coach

--- lib/comedy_shows/scraper.rb
+++ lib/comedy_shows/scraper.rb
@@ -2,7 +2,6 @@ class ComedyShows::Scraper
 
   def self.scrape_shows_list #this provides me with an array of hashes of shows from schedule page with basic information
     doc = Nokogiri::HTML(open("https://www.dcimprov.com/schedule/"))
-    # self.get_page.css("div.box.main") >> How to incorporate the first show? Push into the array in the method below?
 
     shows_array = []
 
@@ -23,6 +22,29 @@ class ComedyShows::Scraper
     end
     shows_array
   end
+
+###Attemped to re-facter and had an error
+    # shows_array = doc.css("div.leading").collect do |s|
+    #     #  if condition met  / not met, then skip
+    #     #    then create Hash
+    #     #    else
+    #     #      skip to next show
+    #          if s.css("h2 a")[0] != nil || s.css("h2 a").text != ""
+    #          shows_hash = {
+    #          :name => s.css("h2 a").text,
+    #          :description => s.css("p").text,
+    #          :date => s.css("span.scheduledate").text,
+    #          :month => s.css("span.scheduledate").text.slice(0..2).downcase, #1st 3 letters of the month, e.g. "Feb"
+    #          :url => s.css("h2 a")[0]['href']
+    #           }
+    #          end
+    #        shows_hash
+    #      end
+    #      shows_array
+    #    end
+
+
+
 
 
   def self.scrape_show_details(profile_url) #produces a hash of details on one specific show
