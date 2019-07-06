d74dec57de49b25e7c4665d72539709ece087bad
Ariana <ariana.berger@gmail.com>
2019-07-06 18:54:15 -0400

    Fix scrape shows list after webpage was updated

--- lib/comedy_shows/scraper.rb
+++ lib/comedy_shows/scraper.rb
@@ -1,48 +1,24 @@
+require 'pry'
 class ComedyShows::Scraper
 
   def self.scrape_shows_list #this provides me with an array of hashes of shows from schedule page with basic information
     doc = Nokogiri::HTML(open("https://www.dcimprov.com/schedule/"))
-
     shows_array = []
 
-    doc.css("div.leading").each do |s|
+    doc.css("div.lead").each do |s|
       url = s.css("h2 a")[0]['href'] unless s.css("h2 a")[0] == nil
-      # if s.css("h2 a").text == " "
-      #   name = s.css("h3 a").text
-      # else name = s.css("h2 a").text
-      # end
       shows_hash = {
-        :name => s.css("h2 a").text,
+        :name => s.css("h1 a").text,
         :description => s.css("p").text,
-        :date => s.css("span.scheduledate").text,
-        :month => s.css("span.scheduledate").text.slice(0..2).downcase, #1st 3 letters of the month, e.g. "Feb"
+        :date => s.css("h3").text,
+        :month => s.css("h3").text.slice(0..2).downcase, #1st 3 letters of the month, e.g. "Feb"
         :url => url
       }
+
       shows_array << shows_hash if shows_hash[:name] != "" && shows_hash[:url] != ""
     end
     shows_array
   end
-
-###Attemped to re-facter and had an error
-    # shows_array = doc.css("div.leading").collect do |s|
-    #     #  if condition met  / not met, then skip
-    #     #    then create Hash
-    #     #    else
-    #     #      skip to next show
-    #          if s.css("h2 a")[0] != nil || s.css("h2 a").text != ""
-    #          shows_hash = {
-    #          :name => s.css("h2 a").text,
-    #          :description => s.css("p").text,
-    #          :date => s.css("span.scheduledate").text,
-    #          :month => s.css("span.scheduledate").text.slice(0..2).downcase, #1st 3 letters of the month, e.g. "Feb"
-    #          :url => s.css("h2 a")[0]['href']
-    #           }
-    #          end
-    #        shows_hash
-    #      end
-    #      shows_array
-    #    end
-
 
   def self.scrape_show_details(profile_url) #produces a hash of details on one specific show
     doc = Nokogiri::HTML(open(profile_url))
